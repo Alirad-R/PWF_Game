ba202ba0773631bab4fa600db56241251eba92e4
MahdiOTET <mahdiemami@ut.ac.ir>
2023-12-06 01:51:11 +0330

    Added texture folder and player texture

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,51 @@
 #include <iostream>
-#include "../header/Player.h"
 #include <SFML/Graphics.hpp>
+#include <fstream>
+#include <cctype>
+#include <string>
 using namespace std;
 
 int main() {
+
+	std::ifstream openfile("map.txt");
+	sf::Texture tileTexture;
+	sf::Sprite tiles;
+
+	sf::Vector2i map[100][100];
+	sf::Vector2i loadCounter = sf::Vector2i(0, 0);
+	if (openfile.is_open())
+	{
+		std::string tileLocation;
+		openfile >> tileLocation;
+		tileTexture.loadFromFile(tileLocation);
+		tiles.setTexture(tileTexture);
+		while (!openfile.eof())
+		{
+			std::string str;
+			openfile >> str;
+			char x = str[0], y = str[2];
+			if (!isdigit(x) || !isdigit(y))
+			{
+				map[loadCounter.x][loadCounter.y] = sf::Vector2i(-1, -1);
+			}
+			else
+			{
+				map[loadCounter.x][loadCounter.y] = sf::Vector2i(x - '0', y - '0');
+			}
+
+			if (openfile.peek() == '\n')
+			{
+				loadCounter.x = 0;
+				loadCounter.y = loadCounter.y + 1;
+			}
+			else
+			{
+				loadCounter.x = loadCounter.x + 1;
+			}
+
+		}
+		loadCounter.y = loadCounter.y + 1;
+	}
 
 	const int Screen_width = 1000;
 	const int Screen_height = 1000;
@@ -22,6 +64,19 @@ int main() {
 		}
 
 		window.clear();
+
+		for (int i = 0; i < loadCounter.x; i++)
+		{
+			for (int j = 0; j < loadCounter.y; j++)
+			{
+				if(map[i][j].x != -1 && map[i][j].y != -1){
+					tiles.setPosition(i * 53, j * 53);
+					tiles.setTextureRect(sf::IntRect(map[i][j].x * 53, map[i][j].y * 53, 53, 53));
+					window.draw(tiles);
+				}
+			}
+		}
+
 		window.display();
 	}
 }
